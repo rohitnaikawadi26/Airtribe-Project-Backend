d9fc31c22576b0c246db726146216b569f7edca3
rohitnaikawadi26 <130615127+rohitnaikawadi26@users.noreply.github.com>
2024-03-10 15:59:16 +0530

    Create courseRoutes.js
    Routes in Backend

---
+++ routes/courseRoutes.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+const courseController = require('../controllers/courseController');
+
+router.post('/', courseController.createCourse);
+router.put('/:id', courseController.updateCourse);
+
+module.exports = router;
